7381968c7fb6d11b027ac9a0b0a5c766b3a7c8ea
joahan david <johanarellano33@gmail.com>
2025-10-07 07:20:21 -0700

    agregamos funciones de quien dijo

--- servidormulti/src/main/java/servidormulti/UnCliente.java
+++ servidormulti/src/main/java/servidormulti/UnCliente.java
@@ -1,49 +1,41 @@
 package servidormulti;
-
+ 
 import java.io.BufferedReader;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.Socket;
-
+ 
 public class UnCliente implements Runnable {
-
     final DataOutputStream salida;
     final BufferedReader teclado = new BufferedReader(new InputStreamReader(System.in));
     final DataInputStream entrada;
-
+ 
     UnCliente(Socket s) throws IOException {
         salida = new DataOutputStream(s.getOutputStream());
         entrada = new DataInputStream(s.getInputStream());
     }
-
+ 
     @Override
     public void run() {
-        String mensaje;
         while (true) {
             try {
-                mensaje = entrada.readUTF();
-
-                if (mensaje.startsWith("")) {
-                    String[] partes = mensaje.split(" ");
-                    String aQuien = partes[0].substring(1); 
+                String mensaje = entrada.readUTF();
+ 
+                if (mensaje.startsWith("@")) {
+                    String[] partes = mensaje.split(" ", 2);
+                    String aQuien = partes[0].substring(1);
                     UnCliente cliente = ServidorMulti.clientes.get(aQuien);
-
-                    if (cliente != null) {
-                        cliente.salida.writeUTF(mensaje);
-                    } else {
-                        salida.writeUTF("Cliente " + aQuien + " no encontrado.");
-                    }
-
-                } else {
-                    for (UnCliente cliente : ServidorMulti.clientes.values()) {
-                        cliente.salida.writeUTF(mensaje);
-                    }
+                    cliente.salida.writeUTF(mensaje);
+                    return;
                 }
+ 
+                for (UnCliente cliente : ServidorMulti.clientes.values()) {
+                    cliente.salida.writeUTF(mensaje);
+                }
+ 
             } catch (IOException ex) {
-                System.err.println("Error al recibir/enviar mensaje: " + ex.getMessage());
-                break;
             }
         }
     }
