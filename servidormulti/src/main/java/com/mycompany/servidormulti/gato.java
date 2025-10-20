a429626866f27617b8d6115a50d51d74e9b5a476
joahan david <johanarellano33@gmail.com>
2025-10-20 07:42:20 -0700

    agregamos clase de gato y variables

---
+++ servidormulti/src/main/java/com/mycompany/servidormulti/gato.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.servidormulti;
+
+public class gato {
+    private char[][] tablero;
+    private String jugador1;
+    private String jugador2;
+    private String turnoActual;
+    private char simboloJugador1;
+    private char simboloJugador2; 
+    private boolean juegoTerminado;
+    private String ganador;
+}
