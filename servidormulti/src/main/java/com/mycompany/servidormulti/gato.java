bc2b67b22984f5ecaa03d5f26727412250d35ecd
joahan david <johanarellano33@gmail.com>
2025-10-22 07:08:10 -0700

    agregue sombilos y iniciando tablero

--- servidormulti/src/main/java/com/mycompany/servidormulti/gato.java
+++ servidormulti/src/main/java/com/mycompany/servidormulti/gato.java
@@ -10,4 +10,31 @@ public class gato {
     private char simboloJugador2; 
     private boolean juegoTerminado;
     private String ganador;
+    
+   
+   public gato(String jugador1, String jugador2, boolean jugador1Empieza) {
+        this.tablero = new char[3][3];
+        this.jugador1 = jugador1;
+        this.jugador2 = jugador2;
+        this.juegoTerminado = false;
+        this.ganador = null;
+        
+      
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                tablero[i][j] = '-';
+            }
+        }
+        
+       
+        if (jugador1Empieza) {
+            this.simboloJugador1 = 'X';
+            this.simboloJugador2 = 'O';
+            this.turnoActual = jugador1;
+        } else {
+            this.simboloJugador1 = 'O';
+            this.simboloJugador2 = 'X';
+            this.turnoActual = jugador2;
+        }
+    }
 }
